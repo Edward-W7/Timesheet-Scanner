92488f46724a3201f85af5d028a6f8614ce0cdae
Edward Wang <115112114+Edward-W7@users.noreply.github.com>
2024-08-08 11:27:06 -0400

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -43,11 +43,18 @@ def reversename(name):
 
 
 header = True
-path = input('Input your folder directory \n')
+path = input('Input your folder directory \nTool version: 1.1 Build: 2024/08/08 \n')
 # path = '/Users/edwardwang/Desktop/test'
 dir_list = os.listdir(path)
-with open(f'{path}/timesheetlog.csv', 'w') as csvfile:
-    csvfile.close()
+fileNotOpen = True
+while fileNotOpen:
+    try:
+        with open(f'{path}/timesheetlog.csv', 'w') as csvfile:
+            csvfile.close()
+            fileNotOpen = False
+    except PermissionError:
+        print("The Excel file is currently open on an application. Please close it.")
+        path = input('Input your folder directory \n')
 
 for filename in dir_list:
     print('----------------------------')
@@ -56,44 +63,60 @@ for filename in dir_list:
         pdfFileObj = open(file, 'rb')
         pdfReader = PyPDF2.PdfReader(pdfFileObj)
         text = pdfReader.pages[0].extract_text()
-
+        print(text)
         with open(f'{path}/timesheetlog.csv', 'a', newline='\n') as csvfile:
-            fieldnames = ['filename', 'year', 'month', 'days', 'name', 'formatname']
+            fieldnames = ['formatname', 'year', 'month', 'days', 'hours', 'report run', 'version', 'name', 'filename',
+                          'SOW number']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             if header:
-                info = {'name': 'Name',
-                        'formatname': 'Formatted Name',
+                info = {'name': 'Original Name',
+                        'report run': 'Report Run',
+                        'version': 'Version',
+                        'formatname': 'Name',
                         'days': 'Working Days',
                         'month': 'Month',
                         'year': 'Year',
-                        'filename': 'File Name'
+                        'SOW number': 'SOW Number',
+                        'filename': 'File Name',
+                        'hours': 'Hours'
                         }
                 writer.writerow(info)
 
             if 'Fee for Service (FFS) Resource Timesheet' not in text:
                 info = {'name': 'This PDF is not a Timesheet',
                         'formatname': '',
+                        'report run': '',
+                        'version': '',
                         'days': '',
                         'month': '',
                         'year': '',
+                        'SOW number': '',
+                        'hours': '',
                         'filename': filename.encode('ascii', errors='replace').decode().strip()
                         }
             else:
                 info = {
-                    'days': findline("Unauthorized Additional \n", -1),
-                    'name': findline('Resource Name\n', 2),
+                    'report run': findline("REPORT RUN", 0)[10:],
+                    'version': findline("REPORT RUN", 1),
+                    'days': findline("Unauthorized Additional", -1),
+                    'name': findline('Resource Name', 2),
                     'month': findline('Month\n', 5),
                     'year': findline('Year\n', 5),
-                    'filename': filename
+                    'SOW number': findline('SOW Number\n', 2),
+                    'filename': filename,
+                    'hours': findline("Total Billable Days\n", -1)
                 }
-
+                if float(info['days']) != float(findline("Total Billable Days\n", -1)) / 7.25:
+                    print("Warning: total days is not equal to total hours divided by 7.25")
+                    print("Total hours:", findline('Total Billable Days\n', -1))
                 if info['month'].isdigit():
                     info['month'] = findline('Month\n', 4)
                 if not info['year'].isdigit():
                     info['year'] = findline('Year\n', 4)
                 if 'Organization' in info['name']:
                     info['name'] = findline('Resource Name\n', 1)
-
+                if ',' in info['SOW number']:
+                    info['SOW number'] = ''
                 info['formatname'] = reversename(info['name'])
                 for key, value in info.items():
                     info[key] = value.encode('ascii', errors='replace').decode().strip()
@@ -107,3 +130,4 @@ for filename in dir_list:
         pdfFileObj.close()
     elif not file.endswith('timesheetlog.csv'):
         print('given file was not a pdf')
+input('Input any key to close')
